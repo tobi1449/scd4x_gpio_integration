03bc1b28f72e6e2a48843cba42e1a64fb3aff8d2
Tobias Zimmermann <t-zimmermann@mailbox.org>
2023-02-05 00:26:49 +0100

    Add scd4x python api to requirements Add asyncer Make all sync api methods of the scd4x api awaitable via asyncer

--- custom_components/scd4x_gpio_integration/scd4x_api.py
+++ custom_components/scd4x_gpio_integration/scd4x_api.py
@@ -3,15 +3,44 @@ import asyncio
 from typing import Optional
 
 import async_timeout
+from asyncer import asyncify
 
-from .sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection
-from .sensirion_i2c_scd import Scd4xI2cDevice
+from sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection
+from sensirion_i2c_scd import Scd4xI2cDevice
 
 TIMEOUT = 15
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 HEADERS = {"Content-type": "application/json; charset=UTF-8"}
+
+
+def stop_periodic_measurement(scd4x: Scd4xI2cDevice):
+    scd4x.stop_periodic_measurement()
+
+
+def reinit(scd4x: Scd4xI2cDevice):
+    scd4x.reinit()
+
+
+def read_serial_number(scd4x: Scd4xI2cDevice):
+    scd4x.read_serial_number()
+
+
+def start_periodic_measurement(scd4x: Scd4xI2cDevice):
+    scd4x.start_periodic_measurement()
+
+
+def get_data_ready_status(scd4x: Scd4xI2cDevice) -> bool:
+    return scd4x.get_data_ready_status()
+
+
+def read_measurement(scd4x: Scd4xI2cDevice) -> tuple:
+    return scd4x.read_measurement()
+
+
+def set_sensor_altitude(scd4x: Scd4xI2cDevice, sensor_altitude):
+    scd4x.set_sensor_altitude(sensor_altitude)
 
 
 class SCD4xAPI:
@@ -37,17 +66,17 @@ class SCD4xAPI:
             self._scd4x = Scd4xI2cDevice(self._i2c_connection)
 
             _LOGGER.info("Stopping periodic measurements.")
-            await self._scd4x.async_stop_periodic_measurement()
+            await asyncify(stop_periodic_measurement)(scd4x=self._scd4x)
             await asyncio.sleep(1)
             _LOGGER.info("Reinitializing device.")
-            await self._scd4x.async_reinit()
+            await asyncify(reinit)(scd4x=self._scd4x)
             await asyncio.sleep(5)
 
             _LOGGER.info("Reading serial number.")
-            serial = await self._scd4x.async_read_serial_number()
+            serial = await asyncify(read_serial_number)(scd4x=self._scd4x)
             _LOGGER.info(f"Serial Number {serial}")
 
-            await self._scd4x.async_start_periodic_measurement()
+            await asyncify(start_periodic_measurement)(scd4x=self._scd4x)
             _LOGGER.info("Starting periodic measurements.")
 
             await asyncio.sleep(1)
@@ -60,7 +89,7 @@ class SCD4xAPI:
             _LOGGER.info("Stop API called.")
             try:
                 if self._scd4x is not None:
-                    await self._scd4x.async_stop_periodic_measurement()
+                    await asyncify(stop_periodic_measurement)(scd4x=self._scd4x)
             except Exception as exception:
                 _LOGGER.warning(f"Unable to stop SCD4x perodic measurements: {exception}")
             try:
@@ -77,11 +106,11 @@ class SCD4xAPI:
 
         async with async_timeout.timeout(TIMEOUT):
 
-            while not await self._scd4x.async_get_data_ready_status():
+            while not await asyncify(get_data_ready_status)(scd4x=self._scd4x):
                 await asyncio.sleep(0.2)
 
             _LOGGER.info("Data ready, getting data")
-            co2, temp, humidity = await self._scd4x.async_read_measurement()
+            co2, temp, humidity = await asyncify(read_measurement)(scd4x=self._scd4x)
             _LOGGER.info(f"Data available: {co2.co2};{temp.degrees_celsius};{humidity.percent_rh}")
             return round(co2.co2, 2), round(temp.degrees_celsius, 2), round(humidity.percent_rh, 2)
 
@@ -89,11 +118,4 @@ class SCD4xAPI:
         if not self._connection_established:
             return
 
-        await self._scd4x.async_set_sensor_altitude(altitude)
-
-    async def async_get_serial_number(self):
-        if not self._connection_established:
-            return False
-
-        async with async_timeout.timeout(TIMEOUT):
-            return await self._scd4x.async_read_serial_number()
+        await asyncify(set_sensor_altitude)(scd4x=self._scd4x, sensor_altitude=altitude)
