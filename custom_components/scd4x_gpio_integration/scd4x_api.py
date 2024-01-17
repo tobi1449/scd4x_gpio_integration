5221b7c2b0ffdad1a29218ac82c218b08ec32981
Tobias Zimmermann <t-zimmermann@mailbox.org>
2024-01-17 21:35:20 +0100

    Remove asyncer dependency

--- custom_components/scd4x_gpio_integration/scd4x_api.py
+++ custom_components/scd4x_gpio_integration/scd4x_api.py
@@ -1,9 +1,9 @@
 import asyncio
+import functools
 import logging
 from typing import Optional
 
 import async_timeout
-from asyncer import asyncify
 from sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection
 from sensirion_i2c_scd import Scd4xI2cDevice
 
@@ -14,44 +14,54 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 HEADERS = {"Content-type": "application/json; charset=UTF-8"}
 
 
-def stop_periodic_measurement(scd4x: Scd4xI2cDevice):
-    scd4x.stop_periodic_measurement()
+async def run_async(method):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, method)
 
 
-def reinit(scd4x: Scd4xI2cDevice):
-    scd4x.reinit()
+async def stop_periodic_measurement(scd4x: Scd4xI2cDevice):
+    await run_async(functools.partial(Scd4xI2cDevice.stop_periodic_measurement, self=scd4x))
 
 
-def read_serial_number(scd4x: Scd4xI2cDevice) -> int:
-    return scd4x.read_serial_number()
+async def reinit(scd4x: Scd4xI2cDevice):
+    await run_async(functools.partial(Scd4xI2cDevice.reinit, self=scd4x))
 
 
-def start_periodic_measurement(scd4x: Scd4xI2cDevice):
-    scd4x.start_periodic_measurement()
+async def read_serial_number(scd4x: Scd4xI2cDevice) -> int:
+    return await run_async(functools.partial(Scd4xI2cDevice.read_serial_number, self=scd4x))
 
 
-def get_data_ready_status(scd4x: Scd4xI2cDevice) -> bool:
-    return scd4x.get_data_ready_status()
+async def start_periodic_measurement(scd4x: Scd4xI2cDevice):
+    await run_async(functools.partial(Scd4xI2cDevice.start_periodic_measurement, self=scd4x))
 
 
-def read_measurement(scd4x: Scd4xI2cDevice) -> tuple:
-    return scd4x.read_measurement()
+async def get_data_ready_status(scd4x: Scd4xI2cDevice) -> bool:
+    return await run_async(functools.partial(Scd4xI2cDevice.get_data_ready_status, self=scd4x))
 
 
-def set_sensor_altitude(scd4x: Scd4xI2cDevice, sensor_altitude):
-    scd4x.set_sensor_altitude(sensor_altitude)
+async def read_measurement(scd4x: Scd4xI2cDevice) -> tuple:
+    return await run_async(functools.partial(Scd4xI2cDevice.read_measurement, self=scd4x))
 
-def get_sensor_altitude(scd4x: Scd4xI2cDevice) -> int:
-    return scd4x.get_sensor_altitude()
 
-def set_temperature_offset(scd4x: Scd4xI2cDevice, temperature_offset):
-    scd4x.set_temperature_offset(temperature_offset)
+async def set_sensor_altitude(scd4x: Scd4xI2cDevice, sensor_altitude):
+    await run_async(functools.partial(Scd4xI2cDevice.set_sensor_altitude, self=scd4x, sensor_altitude=sensor_altitude))
 
-def get_temperature_offset(scd4x: Scd4xI2cDevice) -> float:
-    return scd4x.get_temperature_offset()
 
-def persist_settings(scd4x: Scd4xI2cDevice):
-    scd4x.persist_settings()
+async def get_sensor_altitude(scd4x: Scd4xI2cDevice) -> int:
+    return await run_async(functools.partial(Scd4xI2cDevice.get_sensor_altitude, self=scd4x))
+
+
+async def set_temperature_offset(scd4x: Scd4xI2cDevice, temperature_offset):
+    await run_async(
+        functools.partial(Scd4xI2cDevice.set_temperature_offset, self=scd4x, temperature_offset=temperature_offset))
+
+
+async def get_temperature_offset(scd4x: Scd4xI2cDevice) -> float:
+    return await run_async(functools.partial(Scd4xI2cDevice.get_temperature_offset, self=scd4x))
+
+
+async def persist_settings(scd4x: Scd4xI2cDevice):
+    await run_async(functools.partial(Scd4xI2cDevice.persist_settings, self=scd4x))
 
 
 class SCD4xAPI:
@@ -79,37 +89,37 @@ class SCD4xAPI:
             self._scd4x = Scd4xI2cDevice(self._i2c_connection)
 
             _LOGGER.debug("Stopping periodic measurements.")
-            await asyncify(stop_periodic_measurement)(scd4x=self._scd4x)
+            await stop_periodic_measurement(self._scd4x)
             await asyncio.sleep(1)
 
             _LOGGER.debug("Reading serial number.")
-            serial = await asyncify(read_serial_number)(scd4x=self._scd4x)
+            serial = await read_serial_number(self._scd4x)
             _LOGGER.debug(f"Serial Number {serial}")
 
             should_save = False
 
-            saved_altitude = asyncify(get_sensor_altitude)(scd4x=self._scd4x)
+            saved_altitude = get_sensor_altitude(self._scd4x)
             altitude = self._altitude if self._altitude is not None else 0
             if altitude is not saved_altitude:
                 _LOGGER.debug(f"Setting altitude to {self._altitude}")
-                await asyncify(set_sensor_altitude)(scd4x=self._scd4x, sensor_altitude=self._altitude)
+                await set_sensor_altitude(self._scd4x, self._altitude)
                 should_save = True
 
-            saved_temperature_offset = asyncify(get_temperature_offset)(scd4x=self._scd4x)
+            saved_temperature_offset = get_temperature_offset(self._scd4x)
             temperature_offset = self._temperature_offset if self._temperature_offset is not None else 4.0
             if temperature_offset is not saved_temperature_offset:
                 _LOGGER.debug(f"Setting temperature offset to {self._temperature_offset}")
-                await asyncify(set_temperature_offset)(scd4x=self._scd4x, temperature_offset=self._temperature_offset)
+                await set_temperature_offset(self._scd4x, self._temperature_offset)
                 should_save = True
 
             if should_save is True:
-                await asyncify(persist_settings)(scd4x=self._scd4x)
+                await persist_settings(self._scd4x)
 
             _LOGGER.debug("Reinitializing device.")
-            await asyncify(reinit)(scd4x=self._scd4x)
+            await reinit(self._scd4x)
             await asyncio.sleep(5)
 
-            await asyncify(start_periodic_measurement)(scd4x=self._scd4x)
+            await start_periodic_measurement(self._scd4x)
             _LOGGER.debug("Starting periodic measurements.")
 
             await asyncio.sleep(1)
@@ -122,7 +132,7 @@ class SCD4xAPI:
             _LOGGER.debug("Stop API called.")
             try:
                 if self._scd4x is not None:
-                    await asyncify(stop_periodic_measurement)(scd4x=self._scd4x)
+                    await stop_periodic_measurement(self._scd4x)
             except Exception as exception:
                 _LOGGER.warning(f"Unable to stop SCD4x periodic measurements: {exception}")
             try:
@@ -139,10 +149,10 @@ class SCD4xAPI:
 
         async with async_timeout.timeout(TIMEOUT):
 
-            while not await asyncify(get_data_ready_status)(scd4x=self._scd4x):
+            while not await get_data_ready_status(self._scd4x):
                 await asyncio.sleep(0.2)
 
             _LOGGER.debug("Data ready, getting data")
-            co2, temp, humidity = await asyncify(read_measurement)(scd4x=self._scd4x)
+            co2, temp, humidity = await read_measurement(self._scd4x)
             _LOGGER.debug(f"Data available: {co2.co2};{temp.degrees_celsius};{humidity.percent_rh}")
             return co2.co2, temp.degrees_celsius, humidity.percent_rh
